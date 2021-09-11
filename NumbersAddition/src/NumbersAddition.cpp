0efab6c868af62991020e56d88f62a66ffaa28f8
bhavya <bhavyag67@gmail.com>
2021-09-11 18:22:01 +0530

    Commit 1002

---
+++ NumbersAddition/src/NumbersAddition.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : NumbersAddition.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int a,b;
+	cout<<"Enter two numbers"<<endl;
+	cin>>a>>b;
+	cout<<"Sum of "<<a<<" and "<<b<<" is "<<a+b;
+	return 0;
+}
